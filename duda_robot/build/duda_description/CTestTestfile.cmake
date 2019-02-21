87173bc8b0bfb6cdc26507d879f239e9c0243645
drematrix <divyebhutani@gmail.com>
2019-02-21 17:48:05 +0530

    still testing

--- duda_robot/build/duda_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/dtusdc-zephyr/duda_robot/src/duda_description
-# Build directory: /home/nvidia/dtusdc-zephyr/duda_robot/build/duda_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
