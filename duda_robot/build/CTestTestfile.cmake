c30a9b302bc35d1bdb6f43877f69ae0c55ebed0b
drematrix <divyebhutani@gmail.com>
2019-02-27 03:03:29 +0530

    added teleop in duda_go and cv bridge in duda_vision

--- duda_robot/build/CTestTestfile.cmake
+++ duda_robot/build/CTestTestfile.cmake
@@ -5,6 +5,8 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("duda_vision")
 subdirs("robot_pose_publisher")
 subdirs("duda_description")
 subdirs("duda_gazebo")
+subdirs("duda_go")
